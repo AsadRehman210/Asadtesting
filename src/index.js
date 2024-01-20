5aebc5664271cd8e970c4ce8c94bd96ee86b5350
AsadRehman210 <asadrehman210@gmail.com>
2024-01-20 13:04:59 +0500

    basename

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import {HashRouter as Router } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   
-    <Router basename="/">
+    <Router basename="/Asadtesting">
       <App />
 
     </Router>
